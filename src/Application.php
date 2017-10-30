5695bab504b19c4f9775f2b465eae599b240ac4b
“Raul <raul.kallasmaa@khk.ee>
2017-10-30 17:55:35 +0200

    Create a separate class for CreateBookingController

--- src/Application.php
+++ src/Application.php
@@ -2,21 +2,13 @@
 
 namespace BookingApp;
 
+use BookingApp\Controllers\CreateBookingController;
 use Silex\Application as SilexApplication;
 use Silex\Provider\DoctrineServiceProvider;
 use Silex\Provider\FormServiceProvider;
 use Silex\Provider\LocaleServiceProvider;
 use Silex\Provider\TranslationServiceProvider;
 use Silex\Provider\TwigServiceProvider;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\FormType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\TimeType;
-
 
 /**
  * Custom Application class that hold our application specifix functionality.
@@ -37,8 +29,6 @@ class Application extends SilexApplication
      */
     private function configureServices()
     {
-
-
         $this['debug'] = true;
 
         $this->register(new TwigServiceProvider(), [
@@ -58,14 +48,13 @@ class Application extends SilexApplication
         $this->register(new TranslationServiceProvider(), [
             'translator.domains' => [],
         ]);
-        }
+    }
 
-    
-     // Creates all needed tables to database if they don't exist.
-     
+    /**
+     * Creates all needed tables to database if they don't exist.
+     */
     private function createDBTables()
     {
-
         if (!$this['db']->getSchemaManager()->tablesExist('bookings')) {
             $this['db']->executeQuery("CREATE TABLE bookings (
                 id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -84,52 +73,16 @@ class Application extends SilexApplication
         }
     }
 
-    // Define all used routes and connect a route to its controller.
-     
-    private function configureControllers(){
-
-        $this->get('/bookings/create', function () {
-            $form = $this['form.factory']->createBuilder(FormType::class)
-                ->add('firstName', TextType::class, ['required' => true])
-                ->add('lastName', TextType::class, ['required' => true])
-                ->add('phone', TextType::class, ['required' => true])
-                ->add('email', TextType::class, ['required' => false])
-                ->add('birthday', DateType::class, [
-                    'required' => true,
-                    'widget' => 'single_text',
-                    'format' => 'dd.MM.yyyy',
-                ])
-                ->add('startDate', DateType::class, [
-                    'required' => true,
-                    'widget' => 'single_text',
-                    'format' => 'dd.MM.yyyy',
-                ])
-                ->add('endDate', DateType::class, [
-                    'required' => true,
-                    'widget' => 'single_text',
-                    'format' => 'dd.MM.yyyy',
-                ])
-                ->add('arrivalTime', TimeType::class, ['required' => true])
-                ->add('nrOfPeople', IntegerType::class, ['required' => true])
-                ->add('payingMethod', ChoiceType::class, [
-                    'choices' => [
-                        'cash' => 'cash',
-                        'transfer' => 'transfer',
-                    ],
-                    'required' => true,
-                ])
-                ->add('additionalInformation', TextareaType::class, [
-                    'required' => false
-                ])
-                ->add('submit', SubmitType::class, ['label' => 'Send booking'])
-                ->getForm()
-            ;
-
-            return $this['twig']->render('form.html.twig', [
-                'form' => $form->createView()
-            ]);;
-
-        });
+    /**
+     * Define all used routes and connect a route to its controller.
+     */
+    private function configureControllers()
+    {
+        $this
+            ->match('/bookings/create', new CreateBookingController(
+                $this['form.factory'],
+                $this['twig']
+            ))
+        ;
     }
 }
-?>
